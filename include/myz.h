12be3b3dd5ad80466ef86641e45df95cec5420cb
DimitrisVita <DimitrisVita@mhnyma.com>
2025-02-11 20:58:16 +0200

    Refactor MyzNode structure and update create_archive function for improved metadata handling

--- include/myz.h
+++ include/myz.h
@@ -2,39 +2,35 @@
 
 #include "common.h"
 
+// Header of the archive
 typedef struct {
     char magic[4];  // Identifier "MYZ\0"
+    uint64_t total_bytes;   // Total bytes of the archive
     uint64_t metadata_offset;  // Byte offset to the metadata section
 } MyzHeader;
 
-typedef struct {
-    uint64_t id;
-    uint64_t parent_id;
-    mode_t mode;
-    uid_t uid;
-    gid_t gid;
-    time_t mtime, atime, ctime;
-    off_t size;
-    off_t data_offset;
-    size_t data_length;
-    uint32_t name_len;
-    uint32_t link_target_len;
-    uint64_t inode;  // Inode number for hard link detection
-} MyzNodeSerialized;
+typedef enum {
+    MYZ_NODE_TYPE_FILE,     // Regular file
+    MYZ_NODE_TYPE_DIR,      // Directory
+    MYZ_NODE_TYPE_SYMLINK,  // Symbolic link
+    MYZ_NODE_TYPE_HARDLINK  // Hard link
+} myz_node_type;
 
+// Metadata node
 typedef struct {
-    uint64_t id;            // ID of the entry
-    uint64_t parent_id;     // ID of the parent entry
+    uint64_t id;            // Unique identifier
+    uint64_t parent_id;     // Parent node identifier
+    ino_t inode;            // Inode number
     char *name;             // Name of the file or directory
-    mode_t mode;            // File mode
+    myz_node_type type;     // Type of the entry
     uid_t uid;              // User ID
     gid_t gid;              // Group ID
+    mode_t mode;            // File mode
     time_t mtime, atime, ctime; // Timestamps
-    off_t size;             // File size
-    off_t data_offset;      // Byte offset to the data
-    size_t data_length;     // Length of the data
-    char *link_target;      // Target of the symbolic link
-    uint64_t inode;         // Inode number for hard link detection
+    off_t nodeSize;         // Size of the node
+    off_t dataSize;         // Size of the data section
+    off_t data_offset;      // Byte offset to the data section
+    bool compressed;        // Data is compressed
 } MyzNode;
 
 void create_archive(const char *archiveFile, const char *filePath, bool gzip);
--- src/myz.c
+++ src/myz.c
@@ -4,216 +4,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-typedef struct {
-    MyzNode node;
-    char *filePath;
-    bool gzip;
-} ArchiveEntry;
 
-static uint64_t next_node_id = 1; // Start from 1 (0 is root)
 
-void destroy_archive_entry(void *value) {
-    ArchiveEntry *entry = (ArchiveEntry *)value;
-    free(entry->node.name);
-    free(entry->node.link_target);
-    free(entry->filePath);
-    free(entry);
-}
-
-void add_to_list(List list, const MyzNode *node, const char *filePath, bool gzip) {
-    ArchiveEntry *entry = malloc(sizeof(ArchiveEntry));
-    if (!entry) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    entry->node = *node;
-    entry->filePath = strdup(filePath);
-    if (!entry->filePath) {
-        perror("strdup");
-        exit(EXIT_FAILURE);
-    }
-    entry->gzip = gzip;
-    list_insert_after(list, list_last(list), entry);
-}
-
-void write_metadata(int archive_fd, const MyzNode *node) {
-    MyzNodeSerialized serialized = {
-        .id = node->id,
-        .parent_id = node->parent_id,
-        .mode = node->mode,
-        .uid = node->uid,
-        .gid = node->gid,
-        .mtime = node->mtime,
-        .atime = node->atime,
-        .ctime = node->ctime,
-        .size = node->size,
-        .data_offset = node->data_offset,
-        .data_length = node->data_length,
-        .name_len = node->name ? strlen(node->name) + 1 : 0,
-        .link_target_len = node->link_target ? strlen(node->link_target) + 1 : 0,
-        .inode = node->inode  // Add this
-    };
-
-    write(archive_fd, &serialized, sizeof(MyzNodeSerialized));
-
-    if (node->name) {
-        write(archive_fd, node->name, serialized.name_len);
-    }
-    if (node->link_target) {
-        write(archive_fd, node->link_target, serialized.link_target_len);
-    }
-}
-
-void write_file_data(int archive_fd, const char *filePath, bool gzip) {
-    int fd = open(filePath, O_RDONLY);
-    if (fd == -1) {
-        perror("open");
-        return;
-    }
-
-    char buffer[4096];
-    ssize_t bytesRead;
-
-    if (gzip) {
-        int gz_fd = dup(fd); // Duplicate to avoid closing the original
-        gzFile gz = gzdopen(gz_fd, "rb");
-        close(fd); // Close the original fd
-        if (!gz) {
-            perror("gzdopen");
-            return;
-        }
-
-        while ((bytesRead = gzread(gz, buffer, sizeof(buffer))) > 0) {
-            write(archive_fd, buffer, bytesRead);
-        }
-
-        gzclose(gz);
-    } else {
-        while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0) {
-            write(archive_fd, buffer, bytesRead);
-        }
-        close(fd);
-    }
-}
-
-void process_directory(List list, const char *dirPath, bool gzip, uint64_t parent_id) {
-    // Open the directory
-    DIR *dir = opendir(dirPath);
-    if (!dir) {
-        perror("opendir");
-        return;
-    }
-
-    // Read the directory entries
-    struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) {
-        // Skip "." and ".." entries
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
-        }
-
-        // Construct the full path of the entry
-        char *fullPath = malloc(strlen(dirPath) + strlen(entry->d_name) + 2);
-        if (!fullPath) {
-            perror("malloc");
-            closedir(dir);
-            return;
-        }
-        sprintf(fullPath, "%s/%s", dirPath, entry->d_name);
-
-        
-        struct stat st;
-        if (lstat(fullPath, &st) == -1) {
-            perror("lstat");
-            free(fullPath);
-            continue;
-        }
-
-        MyzNode node = {
-            .id = next_node_id++,  // Assign unique ID
-            .parent_id = parent_id,
-            .name = strdup(entry->d_name),
-            .mode = st.st_mode,
-            .uid = st.st_uid,
-            .gid = st.st_gid,
-            .mtime = st.st_mtime,
-            .atime = st.st_atime,
-            .ctime = st.st_ctime,
-            .size = st.st_size,
-            .data_offset = 0,  // This will be set later
-            .data_length = 0,  // This will be set later
-            .link_target = NULL,
-            .inode = st.st_ino
-        };
-
-        if (!node.name) {
-            perror("strdup");
-            free(fullPath);
-            continue;
-        }
-
-        if (S_ISLNK(st.st_mode)) {
-            char link_target[PATH_MAX];
-            ssize_t len = readlink(fullPath, link_target, sizeof(link_target) - 1);
-            if (len != -1) {
-                link_target[len] = '\0';
-                node.link_target = strdup(link_target);
-                if (!node.link_target) {
-                    perror("strdup");
-                    free(node.name);
-                    free(fullPath);
-                    continue;
-                }
-            }
-        }
-
-        add_to_list(list, &node, fullPath, gzip);
-
-        if (S_ISDIR(st.st_mode)) {
-            process_directory(list, fullPath, gzip, node.id);
-        }
-
-        free(fullPath);
-    }
-
-    closedir(dir);
-}
-
-void compress_and_write_data(const char *filePath, FILE *archive) {
-    int fd = open(filePath, O_RDONLY);
-    if (fd == -1) {
-        perror("open");
-        return;
-    }
-
-    char buffer[4096];
-    ssize_t bytesRead;
-
-    int gz_fd = dup(fd); // Duplicate to avoid closing the original
-    gzFile gz = gzdopen(gz_fd, "rb");
-    close(fd); // Close the original fd
-    if (!gz) {
-        perror("gzdopen");
-        return;
-    }
-
-    while ((bytesRead = gzread(gz, buffer, sizeof(buffer))) > 0) {
-        fwrite(buffer, 1, bytesRead, archive);
-    }
-
-    gzclose(gz);
-}
-
-void write_metadata_section(FILE *archive, List list) {
-    for (ListNode node = list_first(list); node != NULL; node = list_next(list, node)) {
-        ArchiveEntry *entry = list_value(list, node);
-        write_metadata(archive, &entry->node);
-    }
-}
 
 void create_archive(const char *archiveFile, const char *filePath, bool gzip) {
     // Create a list to store archive entries
-    List list = list_create(destroy_archive_entry);
+    List list = list_create(NULL);
 
     // Get file information for the specified file or directory
     struct stat st;
@@ -223,43 +19,28 @@ void create_archive(const char *archiveFile, const char *filePath, bool gzip) {
         return;
     }
     
-    // Duplicate the file path and extract the base name
-    char *base = basename(strdup(filePath));
-    if (!base) {
-        perror("strdup");
-        list_destroy(list);
-        return;
-    }
-    
     // Initialize the root node for the archive with the base name and file metadata
     MyzNode rootNode = {
-        .id = 0,                // Root node ID is 0
-        .parent_id = 0,         // Root node has no parent
-        .name = strdup(base),   // Set the name to the base name of the file or directory
-        .mode = st.st_mode,     // File mode (permissions)
-        .uid = st.st_uid,       // User ID of the owner
-        .gid = st.st_gid,       // Group ID of the owner
-        .mtime = st.st_mtime,   // Last modification time
-        .atime = st.st_atime,   // Last access time
-        .ctime = st.st_ctime,   // Creation time
-        .size = st.st_size,     // Size of the file
-        .data_offset = 0,       // Data offset (to be set later)
-        .data_length = 0,       // Data length (to be set later)
-        .link_target = NULL,    // Link target (if it's a symbolic link)
-        .inode = st.st_ino      // Inode number
+        .inode = st.st_ino,
+        .id = 0,    // Root node ID is 0
+        .parent_id = 0, // Root node has no parent
+        .name = basename(strdup(filePath)), // Get the base name of the file path
+        .type = S_ISDIR(st.st_mode) ? MYZ_NODE_TYPE_DIR : MYZ_NODE_TYPE_FILE,
+        .uid = st.st_uid,
+        .gid = st.st_gid,
+        .mode = st.st_mode,
+        .mtime = st.st_mtime,
+        .atime = st.st_atime,
+        .ctime = st.st_ctime,
+        .nodeSize = sizeof(MyzNode),
+        .dataSize = 0,  // This will be set later
+        .data_offset = 0    // This will be set later
     };
-
-    free(base);
-
-    if (!rootNode.name) {
-        perror("strdup");
-        list_destroy(list);
-        return;
-    }
 
     add_to_list(list, &rootNode, filePath, gzip);
 
-    if (S_ISDIR(st.st_mode)) {
+    // Process the directory recursively
+    if (rootNode.type == MYZ_NODE_TYPE_DIR) {
         process_directory(list, filePath, gzip, rootNode.id);
     }
 
@@ -272,36 +53,8 @@ void create_archive(const char *archiveFile, const char *filePath, bool gzip) {
 
     MyzHeader header = {
         .magic = "MYZ\0",
-        .metadata_offset = 0,  // This will be set later
-        .data_offset = sizeof(MyzHeader)
+        .total_bytes = 0,  // This will be set later
+        .metadata_offset = 0  // This will be set later
     };
 
-    write(archive_fd, &header, sizeof(header));
-
-    // Phase 1: Collect all data and record offsets
-    off_t current_data_offset = lseek(archive_fd, 0, SEEK_CUR);
-    for (ListNode node = list_first(list); node != NULL; node = list_next(list, node)) {
-        ArchiveEntry *entry = list_value(list, node);
-        if (S_ISREG(entry->node.mode)) {
-            entry->node.data_offset = current_data_offset;
-            write_file_data(archive_fd, entry->filePath, entry->gzip);
-            entry->node.data_length = lseek(archive_fd, 0, SEEK_CUR) - entry->node.data_offset;
-            current_data_offset = lseek(archive_fd, 0, SEEK_CUR);
-        }
-    }
-
-    // Phase 2: Write metadata section
-    header.metadata_offset = lseek(archive_fd, 0, SEEK_CUR);
-    for (ListNode node = list_first(list); node != NULL; node = list_next(list, node)) {
-        ArchiveEntry *entry = list_value(list, node);
-        write_metadata(archive_fd, &entry->node);
-    }
-
-    // Update header.metadata_offset
-    lseek(archive_fd, 0, SEEK_SET);
-    write(archive_fd, &header, sizeof(header));
-
-    close(archive_fd);
-    list_destroy(list);
 }
-
