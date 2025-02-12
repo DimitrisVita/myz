7252cf5c7e638496f19f41ea189c74a29a942c91
DimitrisVita <DimitrisVita@mhnyma.com>
2025-02-12 22:18:41 +0200

    Refactor create_archive function to enhance metadata output and clean up commented code

--- src/myz.c
+++ src/myz.c
@@ -1,5 +1,8 @@
 #include "myz.h"
 
+// Transfer the list to the archive file
+
+
 // Process the directory recursively
 int processDirectory(char *dirPath, List list, uint64_t parent_id, bool gzip) {
     // Open the directory
@@ -77,8 +80,6 @@ void create_archive(char *archiveFile, char **fileList, bool gzip) {
 
     // Process each file and directory in the list
     for (int i = 0; fileList[i] != NULL; i++) {
-        printf("Processing: %s\n", fileList[i]);
-
         // Get file information
         struct stat st;
         if (lstat(fileList[i], &st) == -1) {
@@ -120,56 +121,25 @@ void create_archive(char *archiveFile, char **fileList, bool gzip) {
     while (node != NULL) {
         MyzNode *entry = list_value(list, node);
         printf("Name: %s, Type: %d\n", entry->name, entry->type);
-        if (entry->type == MYZ_NODE_TYPE_DIR) {
-            printf("  Directory Contents: %d\n", entry->dirContents);
-        }
-        // Print path
-        printf("  Path: %s\n", entry->path);
+        
+        // Print all metadata
+        printf("ID: %lu, Parent ID: %lu, Inode: %lu\n", entry->id, entry->parent_id, entry->inode);
+        printf("Path: %s\n", entry->path);
+        printf("UID: %u, GID: %u, Mode: %o\n", entry->uid, entry->gid, entry->mode);
+        printf("Size: %ld, Data Size: %ld\n", entry->nodeSize, entry->dataSize);
+        printf("Data Offset: %ld, Compressed: %d\n", entry->data_offset, entry->compressed);
+        printf("Timestamps: %ld, %ld, %ld\n", entry->mtime, entry->atime, entry->ctime);
+        printf("Directory Contents: %d\n\n", entry->dirContents);
+
+
         node = list_next(list, node);
     }
 
-    // // Open the archive file for writing
-    // int archive_fd = open(archiveFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    // if (archive_fd == -1) {
-    //     perror("open");
-    //     list_destroy(list);
-    //     return;
-    // }
-
-    // // Calculate the byte offset for the metadata section
-    // off_t metadata_offset = calculate_metadata_offset(list, numDirContents);
-
-    // // Calculate the total bytes of the archive
-    // uint64_t total_bytes = calculate_total_bytes(list, metadata_offset);
-
-    // // Initialize the header of the archive
-    // MyzHeader header = {
-    //     .magic = "MYZ",
-    //     .total_bytes = 0,   // This will be set later
-    //     .metadata_offset = 0    // This will be set later
-    // };
-
-    // // Write the header to the archive file
-    // write_header(archive_fd, &header);
-
-    // // Write the data sections to the archive file
-    // write_data_sections(archive_fd, list, gzip);
-
-    // // Write the metadata sections to the archive file
-    // write_metadata_sections(archive_fd, archive_fd, list, metadata_offset, gzip);
+    // Transfer the list to the archive file
     
-    // // Update the header with the total bytes and metadata offset
-    // header.total_bytes = total_bytes;
-    // header.metadata_offset = metadata_offset;
-
-    // // Write the updated header to the archive file
-    // write_header(archive_fd, &header);
-
-    // // Close the archive file
-    // close(archive_fd);
-
+    
     // Destroy the list
-    // list_destroy(list);
+    list_destroy(list);
 }
 
 // void append_archive(char *archiveFile, char *filePath, bool gzip) {
